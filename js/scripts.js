d9d30cc044ad140fb52d526eb2cd0a7e44d1504f
Triknives <50305423+Triknives@users.noreply.github.com>
2019-10-22 17:02:03 -0700

    Update fade in/out functionality. Updates to Site content. Attempt to fix current fade cycle.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     $(".btn1").click(function(){
-        $("img, h3, h2, button").fadeOut();
+        $("img, h3").fadeOut();
     });
     $(".btn2").click(function(){
-        $("img").fadeIn();
+        $("img").fadeIn("img");
           });
-          $(".clickable").click(function() {
+          $(".clickable").click(function("h4") {
               $("#walrus-showing").toggle();
               $("#walrus-hidden").toggle();
             });
